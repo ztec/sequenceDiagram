8c7e1f3608b76ad75aedbb994ef9591fec5f5f38
ztec <ztec@riepr.fr>
2013-01-09 00:59:42 +0100

    add superEditor

--- exec.js
+++ exec.js
@@ -1,4 +1,5 @@
 (function (){
+
     var value = "";
     if (localStorage.value) {
         value = localStorage.value;
@@ -12,10 +13,17 @@
         localStorage.value = value ;
     }
 
-    var test = function () {
-        if ($('#content').val() !== value) {
-            askDraw($('#content').val());
+    var test = function (el) {
+        var val = "" ;
+        if(el){
+            val = el.textarea.value ;
+        }else{
+            val = $('#content').val() ;
         }
+        if (val !== value) {
+            askDraw(val);
+        }
+        //_.delay(test,200);
     }
     var openWin = function(){
         win = window.open(window.location+'?view=42','_blank');
@@ -25,5 +33,23 @@
 
     $('#openWin').on('click', openWin);
     askDraw($('#content').val());
+
+
+    editAreaLoader.init({
+        id: "content"	// id of the textarea to transform
+        ,start_highlight: true	// if start with highlight
+        ,allow_resize: "both"
+        ,allow_toggle: false
+        ,word_wrap: false
+        ,replace_tab_by_spaces:'2'
+        ,language: "en"
+        ,syntax: "uml"
+        ,autocompletion:true
+        ,change_callback:"zoneonChange"
+    });
+
+    test();
+
+    window.zoneonChange = test ;
 
 })();
--- index.php
+++ index.php
@@ -7,16 +7,54 @@
 <body <?php if(isset($_GET['view'])){ ?> class="view" <?php } ?>>
 <?php if(!isset($_GET['view'])){ ?>
 <div id="contentWrap">
-    <textarea id="content" width="400px" height="200px">
-        Alice->bob: hello
-    </textarea>
+<textarea id="content" >title Ceci est le titre de mon histoire
+participant "Un nounours" as nounours
+participant "Le lapin" as lapin
+participant "La june'geule" as jungle
+participant "Le lecteur" as lecteur
+
+
+nounours->jungle : S'y rend
+loop
+  nounours->jungle : vie
+  nounours->jungle : mange
+  nounours->jungle : s'amuse
+  opt
+    nounours->nounours : copule
+    opt très rarement
+      nounours->lapin : utilise
+    end
+  end
+end
+nounours->+lapin : Comment vas tu ?
+lapin->-nounours : Très bien
+nounours->+lapin: Peut tu me faire une "salade de fruit" ?
+note right of lapin : Il est relou !
+lapin->>+jungle : court cercher des fruits
+    jungle-->jungle : Trouve des fraises
+    jungle->>jungle : Trouve des oranges
+    jungle-->>jungle : Trouve des bananes
+    note over jungle : Mélange les trouvailles
+jungle->-lapin : Propose à la ceuillette la salade de fruits
+lapin->jungle : Ceuille la salde
+jungle->lapin : Donne la  salade ;
+alt Haha
+  lapin->nounours : Jette à la figure
+  note right of lapin : Prend ça !
+  nounours->lapin : Paf !
+  note left of nounours : Ca t'aprendra !
+  note over lecteur : WTF ?!
+else "En fait , non !"
+  lapin-->-nounours : partage la salade
+  note over lecteur : Ah, c'est mieux ;-p
+end</textarea>
 </div>
     <button id="openWin" >Open preview</button>
 <?php } ?>
 <div id="result" class="result">
 
 </div>
-
+<script language="Javascript" type="text/javascript" src="edit_area/edit_area_full.js"></script>
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
 <script src="//cdnjs.cloudflare.com/ajax/libs/underscore.js/1.4.2/underscore-min.js"></script>
 <?php if(!isset($_GET['view'])){ ?>
